a92c1d5357b82ed0323961575af26f1aa6a0e7c7
soysan <soy2.san@gmail.com>
2021-01-08 23:31:00 +0900

    add config and class

---
+++ app.js
@@ -0,0 +1,33 @@
+const config = {
+    parentId : "target",
+    url : "https://api.recursionist.io/builder/computers",
+    cpu : {
+        brand : "cpu_brand",
+        model : "cpu_model",
+    },
+    gpu : {
+        brand : "gpu_brand",
+        model : "gpu_model",
+    },
+    ram : {
+        num : "num",
+        brand : "ram_brand",
+        model : "ram_model",
+    },
+    storage : {
+        disk : "disk",
+        storage : "storage",
+        brand : "disk_brand",
+        model : "disk_model",
+    },
+    display : "showPc",
+}
+
+class Controller {
+
+}
+
+class view {
+
+}
+
