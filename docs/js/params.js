90df8964954b2c3e151d72951e3abcc6fd15fe35
Masayuki <soy2.san@gmail.com>
2021-07-26 02:20:58 +0900

    delete url and add pics

--- docs/js/params.js
+++ docs/js/params.js
@@ -25,14 +25,14 @@ const config = {
 };
 
 const imgs = [
-    "https://www.sozai-library.com/wp-content/uploads/2016/04/7606-300x225.jpg",
-    "https://publicdomainq.net/images/201802/20s/publicdomainq-0019100lxx.jpg",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCjyztNf0Tm1M720Kd0RWj6ACMWtjdYvVimg&usqp=CAU",
-    "https://i1.wp.com/sozaikoujou.com/wordpress/wp-content/uploads/2015/04/th_kaden_ca_033.png?w=860&ssl=1",
-    "https://illustimage.com/photo/649.png"
+    "../pics/pc1.jpg",
+    "../pics/pc2.jpg",
+    "../pics/pc3.png",
+    "../pics/pc4.png",
+    "../pics/pc5.png"
 ]
 
-const errorPic = "https://lh3.googleusercontent.com/proxy/VrzDdgv4B77u-hC8ILcB_UE6yBwWpDSrPTWxZAna4mr4pCHvtqSabvhy5PYeQwUx-8vbhN2VHOw";
+const errorPic = "../pics/err.png";
 
 export  {
     config,
